389a0fd973004c69ea40456862e8f52ca92b12fc
Jonah Bodnar <129788519+JonahETB@users.noreply.github.com>
2023-12-04 12:21:33 -0600

    school

--- script.js
+++ script.js
@@ -1,15 +1,58 @@
 "use strict"
-let happy;
+let happy = false;
 
 (function () {
     let driver = {
-        driverTeam: "",
-        randomDriver: "",
+        info: {
+            driverTeam: "",
+            country: "",
+            podiums: "",
+            points: "",
+            tGP: "",
+            WC: "",
+            hRF: "",
+        },
+        name: "",
         guessCheck: function () {
-            console.log("print");
+            let playerGuess = document.getElementById("guess").value;
+            let output = document.getElementById("correct");
+            let trueGuess = playerGuess.toLowerCase();
+            let driverName = this.name.toLowerCase();
+            if (trueGuess == driverName) {
+                output.innerHTML = "CORRECT";
+                happy = true;
+            } else {
+                output.innerHTML = "WRONG TRY AGAIN"
+            }
         },
         hintCheck: function () {
+            const info = Object.keys(this.info);
+            let LengthOfInfo = Math.floor(Math.random() * objectKey.length);
 
+            console.log("send hint.exe")
+        },
+        values: function (obj) {
+            let objectKey = Object.keys(obj);
+            let randomTeamNum = Math.floor(Math.random() * objectKey.length);
+            let randomTeam = objectKey[randomTeamNum];
+
+            let randomDriverNum;
+            do {
+                randomDriverNum = Math.floor(Math.random() * Object.keys(obj[randomTeam]).length);
+              } while (randomDriverNum === 0);
+            let driverTeam = objectKey[randomTeamNum];
+    
+            let randomDriver = Object.keys(obj[randomTeam])[randomDriverNum];
+            let driver = obj[randomTeam][randomDriver];
+
+            this.name = driver.name;
+            this.info.driverTeam = driverTeam;
+            this.info.country = driver.Country;
+            this.info.podiums = driver.Podiums;
+            this.info.points = driver.Points;
+            this.info.tGP = driver.totalGrandPrix;
+            this.info.WC = driver.WC;
+            this.info.hRF = driver.highestRaceFinish;
         }
     }
 
@@ -17,36 +60,26 @@ let happy;
         type:"GET",
         url:"data.json",
         success: function (res) {
-            values(res);
+            driver.values(res);
         }
     });
     
-    function values (obj) {
-        let objectKey = Object.keys(obj);
-        let randomTeamNum = Math.floor(Math.random() * objectKey.length);
-        let randomTeam = objectKey[randomTeamNum];
-
-        let teamKey = Object.keys(obj[randomTeam]);
-        let randomDriverNum;
-        do {
-            randomDriverNum = Math.floor(Math.random() * teamKey.length);
-          } while (randomDriverNum === 0);
-        let driverTeam = objectKey[randomTeamNum];
-        let randomDriver = teamKey[randomDriverNum];
-
-        console.log(obj[randomTeam][randomDriver]);
-        driver.driverTeam = driverTeam;
-        driver.randomDriver = randomDriver;
- 
-
-        document.getElementById("output").innerHTML = JSON.stringify(randomDriver).replace(/"/g, "");
+    function lock () {
+        if (!happy) {
+            return;
+        }
+        let playerGuess = document.getElementById("guess");
+        playerGuess.value = driver.name;
     }
 
     function check (selector) {
-        happy = selector == 0 ? driver.guessCheck() : driver.hintCheck() ;
+        if (happy) {
+            return;
+        }
+        let somthing = selector == 0 ? driver.guessCheck() : driver.hintCheck();
     }
-
     console.log(driver);
 
     window.check = check;
+    window.lock = lock;
   })();
