99f356b6b551ae7a57f4cd80484da9b6bf025742
Jonah Bodnar <129788519+JonahETB@users.noreply.github.com>
2023-12-04 08:12:08 -0600

    commit monday

--- script.js
+++ script.js
@@ -1,15 +1,52 @@
+"use strict"
+let happy;
 
-fetch("data.json").then(function (res) {
-    return res.json();
-}).then(function (obj) {
-    checkStats(obj);
-}).catch(function (error) {
-    console.log(`somethign went wrong with the data ${error}`);
-})
+(function () {
+    let driver = {
+        driverTeam: "",
+        randomDriver: "",
+        guessCheck: function () {
+            console.log("print");
+        },
+        hintCheck: function () {
 
-function checkStats (obj) {
-    console.log(obj);
-    num = Math.floor(Math.random()*(+obj).length+1);
-    console.log(num);
+        }
+    }
 
-}
+    $.ajax({
+        type:"GET",
+        url:"data.json",
+        success: function (res) {
+            values(res);
+        }
+    });
+    
+    function values (obj) {
+        let objectKey = Object.keys(obj);
+        let randomTeamNum = Math.floor(Math.random() * objectKey.length);
+        let randomTeam = objectKey[randomTeamNum];
+
+        let teamKey = Object.keys(obj[randomTeam]);
+        let randomDriverNum;
+        do {
+            randomDriverNum = Math.floor(Math.random() * teamKey.length);
+          } while (randomDriverNum === 0);
+        let driverTeam = objectKey[randomTeamNum];
+        let randomDriver = teamKey[randomDriverNum];
+
+        console.log(obj[randomTeam][randomDriver]);
+        driver.driverTeam = driverTeam;
+        driver.randomDriver = randomDriver;
+ 
+
+        document.getElementById("output").innerHTML = JSON.stringify(randomDriver).replace(/"/g, "");
+    }
+
+    function check (selector) {
+        happy = selector == 0 ? driver.guessCheck() : driver.hintCheck() ;
+    }
+
+    console.log(driver);
+
+    window.check = check;
+  })();
