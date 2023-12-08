8a5504dfcdfb519ff1810ad72beeb18f92de3119
Jonah Bodnar <129788519+JonahETB@users.noreply.github.com>
2023-12-08 10:10:15 -0600

    final

--- script.js
+++ script.js
@@ -2,111 +2,101 @@
 
 let happy = false;
 const dropdown = document.getElementById('dropdown');
-var i = 0;
-var h = 0;
+var i = 0, h = 0;
 
 (function () {
     let driver = {
         info: {
-            driverTeam: "",
-            country: "",
-            podiums: "",
-            points: "",
-            tGP: "",
-            WC: "",
-            hRF: "",
+            driverTeam: "", country: "", podiums: "", points: "", tGP: "", WC: "", hRF: "",
         },
         name: "",
         guessCheck: function () {
-            let result = dropdown.value;
-            let selectedText = document.getElementById(result);
-            let resultText = selectedText.textContent;
-
-            let output = document.getElementById("correct");
-            let driverName = this.name;
-            if (resultText == driverName) {
+            const result = dropdown.value;
+            const selectedText = document.getElementById(result);
+            const resultText = selectedText.textContent;
+            const output = document.getElementById("correct");
+    
+            if (resultText == this.name) {
                 output.innerHTML = "CORRECT";
                 dropdown.disabled = true;
                 happy = true;
             } else {
-                output.innerHTML = "WRONG TRY AGAIN"
+                output.innerHTML = "WRONG TRY AGAIN";
             }
         },
         hintCheck: function () {
-            const hText = [`You're 1st hint is that the driver has entered ${this.info.tGP} grand prix`,
-                        `You're 2nd hint is that the driver is from ${this.info.country}`,
-                        `You're 3rd hint is that the driver has ${this.info.points} points`,
-                        `You're 4th hint is that the driver has ${this.info.WC} world championship`,
-                        `You're 5th hint is that the driver has ${this.info.podiums} podiums`,
-                        `You're 6th hint is that the driver drivers for ${this.info.driverTeam}`,
-                        `You're 7th hint is that the driver highest finish is ${this.info.hRF}`];
-
-            const text = (h >= 0 && h < hText.length) ? hText[h] : '';
-            h++;  
-            const newHint = document.createElement("p");  
+            const hText = [
+                `You're 1st hint is that the driver `,`You're 2nd hint is that the driver `,`You're 3rd hint is that the driver `,
+                `You're 4th hint is that the driver `,`You're 5th hint is that the driver `,`You're 6th hint is that the driver `,
+                        `You're 7th hint is that the driver `
+                    ],
+            finalText = [`has entered ${this.info.tGP} grand prix`,`is from ${this.info.country}`,`has ${this.info.points} points`,
+                `has ${this.info.WC} world championship`,`has ${this.info.podiums} podiums`,`drivers for ${this.info.driverTeam}`,
+                `highest finish is ${this.info.hRF}`];
+    
+            const text = (h >= 0 && h < hText.length) ? `${hText[h]}${finalText[h++]}`: '';
+            const newHint = document.createElement("p");
             const textNode = document.createTextNode(text);
             newHint.appendChild(textNode);
             output.appendChild(newHint);
         },
         values: function (obj) {
-            let objectKey = Object.keys(obj);
-            let randomTeamNum = Math.floor(Math.random() * objectKey.length);
-            let randomTeam = objectKey[randomTeamNum];
-
+            const objectKey = Object.keys(obj);
+            const randomTeamNum = Math.floor(Math.random() * objectKey.length);
+            const randomTeam = objectKey[randomTeamNum];
+    
             let randomDriverNum;
             do {
                 randomDriverNum = Math.floor(Math.random() * Object.keys(obj[randomTeam]).length);
-              } while (randomDriverNum === 0);
-            let driverTeam = objectKey[randomTeamNum];
+            } while (randomDriverNum === 0);
     
-            let randomDriver = Object.keys(obj[randomTeam])[randomDriverNum];
-            let driver = obj[randomTeam][randomDriver];
-
+            const randomDriver = Object.keys(obj[randomTeam])[randomDriverNum];
+            const driver = obj[randomTeam][randomDriver];
+    
             this.name = driver.name;
-            this.info.driverTeam = driverTeam;
-            this.info.country = driver.Country;
-            this.info.podiums = driver.Podiums;
-            this.info.points = driver.Points;
-            this.info.tGP = driver.totalGrandPrix;
-            this.info.WC = driver.WC;
-            this.info.hRF = driver.highestRaceFinish;
-            this.iterateObject(obj)
+            this.info = {
+                driverTeam: obj[randomTeam].team,
+                country: driver.Country,
+                podiums: driver.Podiums,
+                points: driver.Points,
+                tGP: driver.totalGrandPrix,
+                WC: driver.WC,
+                hRF: driver.highestRaceFinish,
+            };
+    
+            this.iterateObject(obj);
         },
         iterateObject: function (obj) {
-            var option = document.createElement("option");
-            for (var key in obj) {
-                if (obj.hasOwnProperty(key)) {
-                    if (typeof obj[key] === 'object' && obj[key] !== null) {
-                        // If the value is an object, recursively call the function
-                        this.iterateObject(obj[key]);
-                    } else if (key === 'name') {
-                        i++;
-                        // If the key is 'name', log the value
-                        option.text = obj[key];
-                        option.value = "option" + (i);
-                        option.id = "option" + (i);
-                        dropdown.appendChild(option);
-                    } 
+            const option = document.createElement("option");
+            for (const key in obj) {
+                if (obj.hasOwnProperty(key) && key === 'name') {
+                    i++;
+                    option.text = obj[key];
+                    option.value = "option" + i;
+                    option.id = "option" + i;
+                    dropdown.appendChild(option);
+                } else if (typeof obj[key] === 'object' && obj[key] !== null) {
+                    this.iterateObject(obj[key]);
                 }
             }
         }
+    };
 
-    }
     $.ajax({
         type:"GET",
         url:"data.json",
         success: function (res) {
             driver.values(res);
+        },
+        error: function (error) {
+            console.log(error);
         }
     });
 
-    function check (selector) {
-        if (happy) {
-            return;
+ function check(selector) {
+        if (!happy) {
+            selector === 0 ? driver.guessCheck() : driver.hintCheck();
         }
-        let somthing = selector == 0 ? driver.guessCheck() : driver.hintCheck();
-    }
-    console.log(driver);
-
+    }    
     window.check = check;
   })();
